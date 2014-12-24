923fb5051b2492c10f2cb7ccb462e1e9cce813c1
Manash Taunk <maannaash@gmail.com>
2014-12-23 20:40:46 -0600

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Manash Taunk on 12/23/14.
+//  Copyright (c) 2014 Manash Taunk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Manash Taunk on 12/23/14.
+//  Copyright (c) 2014 Manash Taunk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
