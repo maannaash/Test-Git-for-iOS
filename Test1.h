f7ede5e54107b2e7a9a2fafc35c28fd373e206a1
Manash Taunk <maannaash@gmail.com>
2014-12-24 19:14:44 -0600

    added a test class

---
+++ Test1.h
@@ -0,0 +1,13 @@
+//
+//  Test1.h
+//  Git Test
+//
+//  Created by Manash Taunk on 12/24/14.
+//  Copyright (c) 2014 Manash Taunk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test1 : NSObject
+
+@end
---
+++ Test1.m
@@ -0,0 +1,13 @@
+//
+//  Test1.m
+//  Git Test
+//
+//  Created by Manash Taunk on 12/24/14.
+//  Copyright (c) 2014 Manash Taunk. All rights reserved.
+//
+
+#import "Test1.h"
+
+@implementation Test1
+
+@end
